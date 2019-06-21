88c51574a97cffd64c2006cb57086efebe09757f
zxx <zhou15051852560@163.com>
2019-06-21 15:16:57 +0800

    Update $953_VerifyinganAlienDictionary.java

--- java/hashmap/$953_VerifyinganAlienDictionary.java
+++ java/hashmap/$953_VerifyinganAlienDictionary.java
@@ -1,5 +1,11 @@
 package array;
 
+/**
+*因为给出的order只有小写，所以可以定义一个数组，将order对应的索引值放入数组，索引值越小优先级越高。然后两两比较words中的单词，比较过程中有三种情况：
+1.前一个单词的字符优先级大于后一个单词的字符，直接跳出，比较后面的单词
+2.前一个单词的字符优先级小于后一个单词字符，不符合要求，return false
+3.两个字符相等，统计相等字符数，判断是否前一个单词长度>后一个单词（如apple和app）
+*/
 public class $953_VerifyinganAlienDictionary {
     public boolean isAlienSorted(String[] words, String order) {
         //1.将顺序按照对应的索引放入Map，字符对应的索引值越小表示顺序越靠前
