1c867501c355c078769e421553e1fd0798988409
Juice Zhou <zhou15051852560@163.com>
2019-06-25 21:50:54 +0800

    Update $36_ValidSudoku.java

--- java/hashmap/$36_ValidSudoku.java
+++ java/hashmap/$36_ValidSudoku.java
@@ -4,13 +4,16 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 /**
- * 一开始是使用Set储存元素，分别遍历每行、每列、每3x3格是否有相同元素，只能合并每行和每列遍历。
+ * 我一开始是使用Set储存元素，分别遍历每行、每列、每3x3格是否有相同元素，只能合并每行和每列遍历。
  * 后来看了题解，可以在一次遍历时判定每行、每列、每3x3格是否有相同元素。
  * 为每行每列每格创建对应的HashSet
  * 如果当前遍历元素的索引为[i,j]，则加入rowSets[i]、colSets[j]、boxSets[(i/3)*3+j/3]
+ *
+ * 还可以使用boolean[][]来解这道题，效率比较高，和第一种方法类似。
  */
 public class $36_ValidSudoku {
-    public boolean isValidSudoku(char[][] chars) {
+    //方法1
+    public boolean isValidSudoku1(char[][] chars) {
         HashSet<Character>[] rowSets = new HashSet[9];
         HashSet<Character>[] colSets = new HashSet[9];
         HashSet<Character>[] boxSets = new HashSet[9];
@@ -33,6 +36,28 @@ public class $36_ValidSudoku {
                     colSets[j].add(chars[i][j]);
                     boxSets[boxIndex].add(chars[i][j]);
                 }
+            }
+        }
+        return true;
+    }
+
+    //方法2
+    public boolean isValidSudoku2(char[][] chars) {
+        boolean[][] rows = new boolean[9][10];
+        boolean[][] cols = new boolean[9][10];
+        boolean[][] boxes = new boolean[9][10];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (chars[i][j] != '.') {
+                    int boxIndex = (i / 3) * 3 + j / 3;
+                    int num = chars[i][j] - '0';
+                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) return false;
+                    rows[i][num] = true;
+                    cols[j][num] = true;
+                    boxes[boxIndex][num] = true;
+                }
+
             }
         }
         return true;
