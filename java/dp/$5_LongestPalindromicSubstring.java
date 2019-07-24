2644225687d3bf139a03622af5b53662ba80b2b6
Juice Zhou <zhou15051852560@163.com>
2019-07-24 19:09:05 +0800

    190724

---
+++ java/dp/$5_LongestPalindromicSubstring.java
@@ -0,0 +1,27 @@
+/**
+ * 动态规划，dp[i][j]表示从i~j的字符串是否是回文串。
+ * 当char[i] == char[j] && (j-i<=2 || dp[i+1][j-1]是回文串)时为true
+ * 创建一个变量来记录最长回文串，最后返回即可
+ * 这里需要注意动态规划中的i一定要小于定于j，所以可以从字符串末尾定下i，保证不会有情况遗漏
+ */
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) return "";
+        String res = "";
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i; j < s.length();j++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                    if (res.length() < j - i + 1) {
+                        res = s.substring(i, j + 1);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
+
+public class $5_LongestPalindromicSubstring {
+}
