598c2c2af6888e8c311500f01c9f279789428d62
zxx <zhou15051852560@163.com>
2019-07-26 16:58:20 +0800

    Update $121_BestTimeToBuyAndSellStock.java

--- java/dp/$121_BestTimeToBuyAndSellStock.java
+++ java/dp/$121_BestTimeToBuyAndSellStock.java
@@ -1,6 +1,6 @@
 //DP，使用sell表示不持有股票的最大利润，hold表示持有股票的最大利润
 //对于第i天不持有股票的最大利润，可以保持第i-1的不持有股票的最大利润不变，也可以卖出第i-1的股票
-//对于第i天的持有股票的最大利润，可以保持第i-1天的持有股票最大利润，也可以买入当前的股票
+//对于第i天的持有股票的最大利润，可以保持第i-1天的持有股票最大利润，也可以买入当前的股票，因为只能买入一次，所以hold=Math.max(hold,-prices[i])
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
