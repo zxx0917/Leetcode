2222ae33c1a179a7ec665370cd6ce07c5bfc35dd
zxx <zhou15051852560@163.com>
2019-07-31 17:18:20 +0800

    190731

--- java/dp/$873_LengthOfLongestFibonacciSubsequence.java
+++ java/dp/$873_LengthOfLongestFibonacciSubsequence.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 /**
- * 状态：dp[i][j]表示从A[i]到A[j]的斐波那契子序列的长度
+ * 状态：dp[i][j]表示从第一个元素为A[i]、第二个元素为A[j]的斐波那契子序列的长度，如[1,2,3,4,5,8]，因为1+2=3，所以dp[2][3]=3；2+3=5，所以dp[2][5]=dp[2][3]+1=4
  * 状态转移方程：对于A[j]，因为0~j-1的元素是递增的，所以创建两个指针，分别指向最小值(left)和最大值(right)，如果A[left]+A[right]=A[j]，则A[right][j]=A[left][right]+1，
  * 如果A[left]+A[right]>A[j]，缩小大值，right--；如果A[left]+A[right]<A[j]，增加小值，left++
  * 初始化：所有数组初始化为2
