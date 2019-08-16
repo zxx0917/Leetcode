ba25d6f021a09b5f57220ae44daa9bf8a0ccf1cf
zxx <zhou15051852560@163.com>
2019-08-16 13:51:46 +0800

    Update $893_GroupsOfSpecialEquivalentStrings.java

--- java/string/$893_GroupsOfSpecialEquivalentStrings.java
+++ java/string/$893_GroupsOfSpecialEquivalentStrings.java
@@ -6,7 +6,7 @@ import java.util.Set;
  * 因为所有 A[i] 都具有相同的长度，所以没有必要考虑长度问题了
  * 奇数位和偶数位上的所有字符分别一致，就是一组，如abcde、cdabe，两者奇数位上均是a/c/e，偶数位上都是b/d，最后都能通过有限次的交换得到对方
  * 搜集每个字符串中奇数位和偶数位的字符，按字典序排好序后使用Set来筛选，
- * 如abcde的奇数位为ace，偶数位为bd，拼接出来加入Set，即acebd；acbde的奇数位为abe，偶数位为cd，最后拼接为abccd，不为同一组
+ * 如abcde的奇数位为ace，偶数位为bd，拼接出来加入Set，即acebd；acbde的奇数位为abe，偶数位为cd，最后拼接为abecd，不为同一组
  */
 class Solution {
     public int numSpecialEquivGroups(String[] A) {
