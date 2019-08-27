f2e033b12592246c5770f1d75f69c1234f671f17
Juice Zhou <zhou15051852560@163.com>
2019-08-27 21:56:14 +0800

    190827

--- java/greedy/$1029_TwoCityScheduling.java
+++ java/greedy/$1029_TwoCityScheduling.java
@@ -2,43 +2,29 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 /**
- * 贪心算法，将数组按照去A地费用和去B地费用的绝对值从小到大排列，线性遍历排序好的数组，对于去A、B的位置还有剩余时，每组取费用小的那个，
- * 如果A没有剩余，只能去B，如果B没有剩余，只能去A
+ * 优化：其实可以直接将数组按照costA-costB排序，前N个元素一定是去A地更划算，再将后面N个放去B即可
  */
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
+        int n = costs.length / 2, res = 0;
         Arrays.sort(costs, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                return Math.abs(o1[0] - o1[1]) - Math.abs(o2[0] - o2[1]);
+                return (o1[0] - o1[1]) - (o2[0] - o2[1]);
             }
         });
-        int a = 0, b = 0, n = costs.length / 2, res = 0;
-        for (int[] cost : costs) {
-            if (a < n && b < n) {
-                //去A地的花费小于去B的花费
-                if (cost[0] < cost[1]) {
-                    a++;
-                    res += cost[0];
-                } else {
-                    //去B的花费小
-                    b++;
-                    res += cost[1];
-                }
-            } else if (a < n) {
-                //B去满了
-                a++;
-                res += cost[0];
-            } else if (b < n) {
-                //A去满了
-                b++;
-                res += cost[1];
+        int i = 0;
+        while (i < 2 * n) {
+            if (i < n) {
+                res += costs[i][0];
+            } else {
+                res += costs[i][1];
             }
+            i++;
         }
         return res;
     }
 }
-
 
 public class $1029_TwoCityScheduling {
 }
