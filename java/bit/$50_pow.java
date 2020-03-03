1658610818d42607f4b986bdd19d07f028caf392
zxx <zhou15051852560@163.com>
2020-03-03 13:26:09 +0800

    Update $50_pow.java

--- java/bit/$50_pow.java
+++ java/bit/$50_pow.java
@@ -3,33 +3,29 @@
  */
 class Solution {
     public double myPow(double x, int n) {
-        if (n == 0) return 1;
+        if (n == 0 || x == 1) return 1;
         if (x == -1) {
-            //如果n为偶数，返回1
-            if ((n & 1) == 0) {
-                return 1;
-            } else {
-                return -1;
-            }
+            //n是偶数
+            if ((n & 1) == 0) return 1;
+            else return -1;
         }
-        if (x == 1) return 1;
-        if (n == Integer.MIN_VALUE) return 0;
-        //递归，如果n为负数，需要处理
+        //判断结果是否需要取倒数，如果需要，则直接先求倒数，然后再递归计算
         if (n < 0) {
             x = 1 / x;
             n = -n;
         }
-        return getPow(x,n);
+        return getPow(x, n);
     }
 
+    //递归求pow
     private double getPow(double x, int n) {
         if (n == 0) return 1;
-        //如果n为偶数
+        double part = getPow(x, n / 2);
+        //1.如果是偶数
         if ((n & 1) == 0) {
-            double part = getPow(x, n / 2);
             return part * part;
         } else {
-            double part = getPow(x, n / 2);
+            //如果是奇数
             return part * part * x;
         }
     }
