6cdd6e95470b89970c1b71764ee0afd160fd514e
zxx <zhou15051852560@163.com>
2020-03-18 21:16:52 +0800

    Update $270_ClosestBinarySearchTreeValue.java

--- java/tree/$270_ClosestBinarySearchTreeValue.java
+++ java/tree/$270_ClosestBinarySearchTreeValue.java
@@ -1,8 +1,9 @@
 /**
- * 中序遍历BST，判断当前节点和target的差值与记录的节点和target差值的大小，根据结果更换记录的结果节点
+ * dfs，使用两个公共变量记录当前最小差值和对应的节点值，根据结果更换记录的结果节点
  */
 public class $270_ClosestBinarySearchTreeValue {
-    int res;
+    double min = Double.MAX_VALUE;
+    int res = 0;
 
     public int closestValue(TreeNode root, double target) {
         if (root == null) return 0;
@@ -12,8 +13,12 @@ public class $270_ClosestBinarySearchTreeValue {
 
     private void helper(TreeNode root, double target) {
         if (root == null) return;
+        if (min > Math.abs(root.val - target)) {
+            min = Math.abs(root.val - target);
+            res = root.val;
+        }
         helper(root.left, target);
-        res = Math.abs(res - target) > Math.abs(root.val - target) ? root.val : res;
         helper(root.right, target);
     }
+
 }
