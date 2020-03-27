eee33e6830d5e1788097d5f58a094ad90667ceb9
zxx <zhou15051852560@163.com>
2020-03-27 10:44:41 +0800

    Update $538_ConvertBSTtoGreaterTree.java

--- java/tree/$538_ConvertBSTtoGreaterTree.java
+++ java/tree/$538_ConvertBSTtoGreaterTree.java
@@ -1,16 +1,15 @@
 /**
- * 从BST的最大节点开始自底向上递归，节点的更新顺序为右->中->左。创建一个全局变量来保存当前节点需要累加的节点数，当前节点需要累加的节点值sum为上一个节点的val+上一个节点需要累加的sum
+ * 从BST的最右边节点开始进行累加，遍历的顺序是右->中->左，当前节点的更新值为当前节点的原有值+上个节点的累加值，所以设定一个全局变量来保存上一个节点，
+ * 逐个累加节点值即可。
  */
 class Solution {
-    int sum = 0;
+    TreeNode pre = null;
 
     public TreeNode convertBST(TreeNode root) {
         if (root == null) return null;
         convertBST(root.right);
-        //更新累加值
-        sum += root.val;
-        //改变节点值
-        root.val = sum;
+        root.val += pre == null ? 0 : pre.val;
+        pre = root;
         convertBST(root.left);
         return root;
     }
