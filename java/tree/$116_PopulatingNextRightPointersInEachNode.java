53b4869cc05c2384faa6e57c264984a105e4a1ef
zxx <zhou15051852560@163.com>
2020-04-07 13:36:40 +0800

    Update $116_PopulatingNextRightPointersInEachNode.java

--- java/tree/$116_PopulatingNextRightPointersInEachNode.java
+++ java/tree/$116_PopulatingNextRightPointersInEachNode.java
@@ -1,6 +1,6 @@
 /**
  * 自顶向下，因为给出的是完美二叉树，所有的节点都是满的，于是，当前节点的左孩子的next为当前节点的右孩子，如果当前节点还有兄弟节点，
- * 则当前节点有孩子的next节点为兄弟节点的左节点，如果当前节点没有兄弟节点则next指向Null
+ * 则当前节点右孩子的next节点为兄弟节点的左节点，如果当前节点没有兄弟节点则next指向Null
  */
 public class $116_PopulatingNextRightPointersInEachNode {
     public Node connect(Node root) {
